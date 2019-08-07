a7d555b2cbb2748d55c4225344942ce223d86996
vdrosato <vasileios.drosatos@cern.ch>
2019-08-07 14:40:50 +0200

    Add Javadocs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,16 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
+/**
+ * KataTenis for Wemanity code challenge
+ * @author vdrosatos
+ */
 public class Main {
 
     private static final Map<Integer, String> pointMappings = new HashMap<>();
 
     static {
+        // Specify mappings between names and points
         pointMappings.put(0, "Love");
         pointMappings.put(15, "Fifteen");
         pointMappings.put(30, "Thirty");
@@ -16,29 +21,36 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        // Initialize the arrays which holding if the player took the point at turn *index*
         final int[] firstPlayersPoints = new int[100];
         final int[] secondPlayersPoints = new int[100];
 
         int turn = 0;
         while (true) {
-            int point = playPoint();
+            int point = playPoint(); // Play point returns 0 or 1 meaning if the first (0) or the second (1) player won the point
             if (point == 0) {
                 firstPlayersPoints[turn] = 1;
             } else {
                 secondPlayersPoints[turn] = 1;
             }
-            Boolean[] isWinner = calculateWinner(firstPlayersPoints, secondPlayersPoints);
+            Boolean[] isWinner = calculateWinner(firstPlayersPoints, secondPlayersPoints); // Calculates if the game it ends (first boolean)
+                                                                                           // and if it is if the first player won (second boolean)
             if (isWinner[0]) {
-                System.out.printf("\n\n*************\n\nWinner is player %d at turn %d\n\n**************\n\n", (isWinner[1] ? 1 : 2), (turn+1));
+                System.out.printf("\n\n*************\n\nWinner is player %d at turn %d\n\n**************\n\n", (isWinner[1] ? 1 : 2), (turn + 1));
                 break;
             }
-            int[] score = calculateScore(firstPlayersPoints, secondPlayersPoints);
+            int[] score = calculateScore(firstPlayersPoints, secondPlayersPoints); // Calculate the score at this turn
             turn++;
-            printScore(score[0], score[1], turn);
+            printScore(score[0], score[1], turn); // Prints the score at console
         }
     }
 
-
+    /**
+     * Calculates if the game is ended and if the first player won
+     * @param firstPlayersPoints, array holding for each turn if first player won the point (0 or 1)
+     * @param secondPlayersPoints, array holding for each turn if first player won the point (0 or 1)
+     * @return Boolean[], boolean array of 2 elements, first boolean if the game is ended, second boolean if first player was winner
+     */
     private static Boolean[] calculateWinner(final int[] firstPlayersPoints, final int[] secondPlayersPoints) {
         int firstsPlayerWins = Arrays.stream(firstPlayersPoints).sum();
         int secondsPlayerWins = Arrays.stream(secondPlayersPoints).sum();
@@ -52,9 +64,14 @@ public class Main {
         }
 
         return new Boolean[]{isGameEnded, firstPlayerWon};
-
     }
 
+    /**
+     * Calculates the score of the game at the specific turn
+     * @param firstPlayersPoints, array holding for each turn if first player won the point (0 or 1)
+     * @param secondPlayersPoints, array holding for each turn if first player won the point (0 or 1)
+     * @return int[], int array of 2 elements, first int the score of first player, secont int the score of second player
+     */
     private static int[] calculateScore(final int[] firstPlayersPoints, final int[] secondPlayersPoints) {
         int firstsPlayerWins = Arrays.stream(firstPlayersPoints).sum();
         int secondsPlayerWins = Arrays.stream(secondPlayersPoints).sum();
@@ -81,6 +98,7 @@ public class Main {
 
         return new int[]{firstPlayersScore, secondPlayerScore};
     }
+
 
     private static void printScore(Integer firstPlayersScore, Integer secondPlayersScore, int turn) {
         System.out.printf("Turn: %d\nScore: %s - %s\n", turn, pointMappings.get(firstPlayersScore), pointMappings.get(secondPlayersScore));
