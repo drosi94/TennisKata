c4521236fc88a1283a937c94ce56a081b202c540
vdrosato <vasileios.drosatos@cern.ch>
2019-09-03 11:59:37 +0200

    Add test case for random play point

---
+++ src/test/java/TestGame.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import tennis.Game;
+
+public class TestGame {
+    // Test use case of playing a round with winner
+    @Test
+    public void test_PlayPoint_ShouldBeInRange01_Success() {
+        Game game = Game.getInstance();
+
+        int point = game.playPoint();
+
+        assertTrue(point == 1 || point == 0);
+    }
+}
