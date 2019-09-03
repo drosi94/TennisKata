28f9199147d2da6345bbf249617610cd5888e396
vdrosato <vasileios.drosatos@cern.ch>
2019-09-03 10:24:54 +0200

    Clean up code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,7 @@
-import tennis.Game;
-
 /**
  * KataTenis for Wemanity code challenge
  *
  * @author vdrosatos
  */
 public class Main {
-    public static void main(String[] args) {
-        Game.getInstance().play();
-    }
 }
--- src/main/java/exception/GameAlreadyFinishedException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class GameAlreadyFinishedException extends Exception {
-    public GameAlreadyFinishedException(String message) {
-        super(message);
-    }
-}
